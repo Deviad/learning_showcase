3bfd9585d97db7b2f151413f5ba3657ada7b2d33
Davide Pugliese <dvd.pugliese@gmail.com>
2018-10-24 15:28:05 +0300

    Add Abstract Factory

---
+++ GofDesignPatterns/src/creational/abstractFactory/Main.java
@@ -0,0 +1,93 @@
+package creational.abstractFactory;
+
+interface ICityCar {
+    String carName();
+}
+
+interface ILuxuryCar {
+    String carName();
+}
+
+interface ICarFactory {
+    ICityCar getCityCar();
+    ILuxuryCar getLuxuryCar();
+}
+
+class JapaneseCityCar implements ICityCar {
+
+    @Override
+    public String carName() {
+        return "Toyota is a city car";
+    }
+}
+
+class ItalianCityCar implements ICityCar {
+
+    @Override
+    public String carName() {
+        return "Fiat is a city car";
+    }
+}
+
+
+class ItalianLuxuryCar implements ILuxuryCar {
+
+    @Override
+    public String carName() {
+        return "Ferrari is a luxury car";
+    }
+}
+
+class JapaneseLuxuryCar implements ILuxuryCar {
+
+    @Override
+    public String carName() {
+        return "Lexus is a luxury car";
+    }
+}
+
+class ItalianCarFactory implements ICarFactory {
+
+    @Override
+    public ICityCar getCityCar() {
+        return new ItalianCityCar();
+    }
+
+    @Override
+    public ILuxuryCar getLuxuryCar() {
+        return new ItalianLuxuryCar();
+    }
+}
+
+
+
+class JapaneseCarFactory implements ICarFactory {
+
+    @Override
+    public ICityCar getCityCar() {
+        return new JapaneseCityCar();
+    }
+
+    @Override
+    public ILuxuryCar getLuxuryCar() {
+        return new JapaneseLuxuryCar();
+    }
+}
+
+public class Main {
+    public static void main(String ...args) {
+        ItalianCarFactory italianCarFactory = new ItalianCarFactory();
+        JapaneseCarFactory japaneseCarFactory = new JapaneseCarFactory();
+
+        ICityCar japaneseCityCar = japaneseCarFactory.getCityCar();
+        ILuxuryCar japaneseLuxuryCar = japaneseCarFactory.getLuxuryCar();
+
+        ICityCar italianCitycar = italianCarFactory.getCityCar();
+        ILuxuryCar italianLuxuryCar = italianCarFactory.getLuxuryCar();
+
+        System.out.println(italianCitycar.carName());
+        System.out.println(italianLuxuryCar.carName());
+        System.out.println(japaneseCityCar.carName());
+        System.out.println(japaneseLuxuryCar.carName());
+    }
+}
